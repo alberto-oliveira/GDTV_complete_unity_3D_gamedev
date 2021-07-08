0e7ab16a4203a975e04f963e2a178615d186c4b4
Alberto Oliveira <alberto.a.oliv@gmail.com>
2021-07-07 22:54:53 -0300

    Latest changes until lecture 13

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0.0f;
+    [SerializeField] float yValue = 0.0f;
+    [SerializeField] float zValue = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Input.GetAxis("Horizontal"), yValue, zValue);   
     }
 }
