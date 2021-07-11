e063c15a1bac0b37296d98c5919cd50969fde2cf
Alberto Oliveira <alberto.a.oliv@gmail.com>
2021-07-10 22:05:53 -0300

    Added movement script with forward thrust and rotation thrust

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    private Rigidbody rb;
+
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+
+        if(Input.GetKey(KeyCode.Space)){
+
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        }
+
+    }
+
+    void ProcessRotation()
+    {
+
+        if(Input.GetKey(KeyCode.A)){
+
+            transform.Rotate(Vector3.forward * rotationThrust * Time.deltaTime);
+
+        }
+
+        else if(Input.GetKey(KeyCode.D)){
+
+            transform.Rotate(-Vector3.forward * rotationThrust * Time.deltaTime);
+
+        }
+
     }
 }
